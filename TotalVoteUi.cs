c7279dcda7cc9fcec2668ae96c1574c46b863872
Hiina <github@hiina.space>
2021-10-26 20:11:29 -0600

    Add top counts to tally and sorted scrollable output
    those scroll fields and content size filters sure are weird.

--- TotalVoteUi.cs
+++ TotalVoteUi.cs
@@ -9,6 +9,7 @@ public class TotalVoteUi : UdonSharpBehaviour
     public VoteState voteState;
     public WorldLog worldLog;
     public UnityEngine.UI.Text text;
+    public UnityEngine.UI.Text[] topUsernames;
     const int MAXSIZE = 512;
     private void log(string msg)
     {
@@ -52,6 +53,7 @@ public class TotalVoteUi : UdonSharpBehaviour
                     cTally = new int[6];
                     candidateCount++;
                 }
+
                 cTally[0] += ((bitset & 1) > 0 ? 1 : 0);
                 cTally[1] += ((bitset & 2) > 0 ? 1 : 0);
                 cTally[2] += ((bitset & 4) > 0 ? 1 : 0);
@@ -63,21 +65,57 @@ public class TotalVoteUi : UdonSharpBehaviour
         }
         var tallyLines = new string[candidateCount];
         var n = 0;
+
+        var top = new int[6];
+        var topTieCount = new int[6];
+        var topCandidate = new string[6];
+
         for (int i = 0; i < MAXSIZE; i++)
         {
             if (candidateUsernames[i] != null)
             {
                 var t = tally[i];
                 // XXX no format alignment in usharp
-                tallyLines[n++] = $"{t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}: {candidateUsernames[i]}";
+                tallyLines[n++] = $"{candidateUsernames[i].PadLeft(20)}: {t[0]} {t[1]} {t[2]} {t[3]} {t[4]} {t[5]}";
+
+                for (int j = 0; j < 6; j++)
+                {
+                    if (t[j] > top[j])
+                    {
+                        topUsernames[j].text = candidateUsernames[i];
+                        topTieCount[j] = 1;
+                        top[j] = t[j];
+                    } else if (t[j] == top[j])
+                    {
+                        // uniformly sample between tied users
+                        topTieCount[j]++;
+                        if (UnityEngine.Random.Range(0, topTieCount[j]) == 0)
+                        {
+                            topUsernames[j].text = candidateUsernames[i];
+                        }
+                    }
+                }
             }
+        }
+
+        // sort tally alphabetically
+        for (int i = 1; i < n; ++i)
+        {
+            var s = tallyLines[i];
+            var j = i - 1;
+            while (j >= 0 && tallyLines[j].CompareTo(s) > 0)
+            {
+                tallyLines[j + 1] = tallyLines[j];
+                j--;
+            }
+            tallyLines[j + 1] = s;
         }
 
         text.text = $"Vote Tally:\n{string.Join("\n", tallyLines)}";
     }
 
-  private
-        int linearProbe(string key, string[] keys)
+    private
+          int linearProbe(string key, string[] keys)
     {
         // XXX negative modulus happens sometimes. might be biased but good enough for here.
         var init = Mathf.Abs(key.GetHashCode()) % MAXSIZE;
