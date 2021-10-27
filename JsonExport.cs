48c5d49430d2fe03be1d5329828779c3be5267ed
Hiina <github@hiina.space>
2021-10-26 19:20:13 -0600

    Add JsonExport
    I played with jq a bit to try to massage the merge but there's
    no simple way to merge same-key values wiht a function sadly.
    i'll have to just use javascript later to make a webpage.

---
+++ JsonExport.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+/// <summary>
+/// Export VoteState as JSON so you can sum multiple instances without double counting.
+/// </summary>
+public class JsonExport : UdonSharpBehaviour
+{
+    public VoteState voteState;
+    public UnityEngine.UI.InputField output;
+
+    // called from button
+    public void DoExport()
+    {
+        // only allow some people to do this
+        //if (Networking.LocalPlayer.displayName != "hiina") return;
+
+        var s = "{";
+        bool hadOne = false;
+        for (int i = 0; i < voteState.voterUsernames.Length; i++)
+        {
+            string voter = voteState.voterUsernames[i];
+            if (voter == "") continue;
+
+            if (hadOne) s += ",";
+            s += $"\"{voter}\":" + "{";
+
+            var splits = voteState.votes[i].Split(',');
+            bool hadTwo = false;
+            for (int j = 0; j < splits.Length; j++)
+            {
+                var vc = splits[j].Split(':');
+                var candidate = vc[0];
+                var bitset = vc[1];
+                if (hadTwo) s += ",";
+                s += $"\"{candidate}\":{bitset}";
+                hadTwo = true;
+            }
+            s += "}";
+            hadOne = true;
+        }
+        output.text = s + "}";
+        Debug.Log(output.text);
+    }
+}
